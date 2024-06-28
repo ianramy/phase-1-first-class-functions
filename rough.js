91f1cd2384f3c0bca286ab5d30e73b678f8447f3
ianramy <imwagore@gmail.com>
2024-06-28 21:38:47 +0300

    origin/master

--- rough.js
+++ rough.js
@@ -30,3 +30,30 @@ function receivesAFunction(callback) {
     returnsANamedFunction,
     returnsAnAnonymousFunction
   };
+// Function receivesAFunction
+function receivesAFunction(callback) {
+    callback();
+}
+
+// Function returnsANamedFunction
+function returnsANamedFunction() {
+    function namedFunction() {
+        console.log('I am a named function!');
+    }
+    return namedFunction;
+}
+
+// Function returnsAnAnonymousFunction
+function returnsAnAnonymousFunction() {
+    return function() {
+        console.log('I am an anonymous function!');
+    }
+}
+
+// Exporting the functions if needed in a module environment
+module.exports = {
+    receivesAFunction,
+    returnsANamedFunction,
+    returnsAnAnonymousFunction
+};
+  
